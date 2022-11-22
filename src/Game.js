bf9bc4c92a79ad10045ee61cedab3228a3faa7b6
Kyle_Home <trong.nguyen@student.vinci.com>
2022-11-22 21:00:10 +0100

    feat: add spare scoring

--- src/Game.js
+++ src/Game.js
@@ -10,7 +10,11 @@ class Game {
     let MAX_TURNS = 10; // 1 turn = 2 rolls
     let j = 0; // rolls index
     for (let i = 0; i < MAX_TURNS; i++) {
-      score += this.rolls[j] + this.rolls[j + 1]; // player rolls 2 times = 1 turn passed
+      if(this.rolls[j] + this.rolls[j + 1]===10){ // spare
+        score+=10+this.rolls[j+2];
+      }else{
+        score += this.rolls[j] + this.rolls[j + 1]; // player rolls 2 times = 1 turn passed
+      }
       j += 2;
     }
     return score;
